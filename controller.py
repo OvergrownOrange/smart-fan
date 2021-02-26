5df297c22853d33d9435a9782389fe55d5bd7072
Adam Knox <adam.knox91@gmail.com>
2021-02-26 23:34:04 +1100

    Added Alerts and Multiprocessing

--- controller.py
+++ controller.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 
-
 import RPi.GPIO as GPIO
 import time
+import requests
+from multiprocessing import Process, Value
+import json
 
-fan = 13        # PWM pin
-sensor = 4     # Temperature sensor
+fan = 13       # PWM pin
+sensor = 4     # Temperature sensorSensor Pin
 
 #GPIO Setup
-#GPIO.setwarnings(False)
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan,GPIO.OUT)
 GPIO.setup(sensor,GPIO.IN)
 
 pwm = GPIO.PWM(fan, 100)
 pwm.start(0)
+
+#Define the upper and lower bounds of the temperature guage
+tempMin = 25
+tempMax = 30
 
 
 def getTemperature():
@@ -25,7 +31,7 @@ def getTemperature():
     tempData = data.split("\n")[1].split(" ")[9]
     temperature = float(tempData[2:])
     temperature = temperature/1000
-    return temperature
+    return int(temperature)
 
 
 def setPWM(duty):
@@ -33,11 +39,15 @@ def setPWM(duty):
 
 
 #Map the temperature range to the PWM range
-def translate(temp, tempMin, tempMax):
+def calcDuty(temp):
     #Set the PWM values
     pwmMin = 20
     pwmMax = 100
     
+    #Sanity check for out of scope temperatures
+    if temp < tempMin: return pwmMin
+    if temp > tempMax: return pwmMax
+
     #Get the ranges
     tempRange = tempMax - tempMin
     pwmRange = pwmMax - pwmMin
@@ -49,22 +59,47 @@ def translate(temp, tempMin, tempMax):
     return int(pwmMin + (valueScaled*pwmRange))
 
 
-def runFan():
-    
-    # TODO: Adjust these to more accurate readings within the rack
-    tempMin = 25
-    tempMax = 40
+def alert(temp):
+    global alerted
+    alerted.value = 1
 
+    # Get the Webhook API key
+    with open('key.json') as json_file:
+        data = json.load(json_file)
+        key = data['key']
+
+    url = 'https://maker.ifttt.com/trigger/rack_temperature/with/key/' + key
+    payload = {'value1' : "temp"}
+    requests.post(url, data=payload)
+    
+    # Delay running this alert over and over
+    time.sleep(1800)  
+    temp = getTemperature()
+    if temp <= tempMax: 
+        alerted.value = 0
+    else:
+        alert(temp)       # Still hot, send another alert!
+
+
+def main():
+    global alerted
     try:
         while True:
             temperature = getTemperature()
-            dutyCycle = translate(temperature, tempMin, tempMax)
+            dutyCycle = calcDuty(temperature)
             setPWM(dutyCycle)
-            time.sleep(60)
-    except KeyboardInterrupt:
-        pass
+            
+            if temperature > tempMax and alerted.value == 0:        # Start a seperate thread for alert
+                p2 = Process(target=alert, args=[temperature])
+                p2.start()
 
-runFan()
+            time.sleep(5)
+    except:
+        pwm.stop()
+        GPIO.cleanup()
 
-pwm.stop()
-GPIO.cleanup()
+
+if __name__ == '__main__':
+    alerted = Value('i', 0)
+    p1 = Process(target=main)
+    p1.start()
